20cd7e5686a0b07d41764755e9e81d1bff8741a8
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2024-04-30 21:17:34 -0400

    Add ResponsivePagination package. Add basic paging functionality

--- app/actions.tsx
+++ app/actions.tsx
@@ -12,17 +12,23 @@ export const getToken = () => {
     .then((payload) => payload.data.token);
 };
 
-export const getMovies = async (token: string) => {
+export const getMovies = async (
+  token: string,
+  options: { page: number; limit: number } = { page: 1, limit: 25 },
+) => {
   if (!process.env.MOVIES_ENDPOINT) {
     throw new Error("Token endpoint is undefined.");
   }
 
   return axios
-    .get(`${process.env.MOVIES_ENDPOINT}/movies`, {
-      headers: {
-        authorization: `Bearer ${token}`,
+    .get(
+      `${process.env.MOVIES_ENDPOINT}/movies?page=${options.page}&limit=${options.limit}`,
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
       },
-    })
+    )
     .then((payload) => payload.data)
     .catch((error) => error.message);
 };
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,41 +1,58 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import ResponsivePagination from "react-responsive-pagination";
 
 import { getToken, getMovies } from "@/app/actions";
 import Movie from "@/app/components/Movie";
 
 const Search = () => {
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(" ");
   const [movies, setMovies] = useState({ data: [], totalPages: 0 });
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    const fetchInitialData = async () => {
+    const fetchToken = async () => {
       const t = await getToken();
       setToken(t);
+    };
 
-      const movies = await getMovies(t);
+    const fetchInitialData = async () => {
+      const movies = await getMovies(token, { page: currentPage, limit: 25 });
       setMovies(movies);
     };
 
-    fetchInitialData();
-  }, []);
-
+    if (token.length > 1) {
+      fetchInitialData();
+    } else {
+      fetchToken();
+      fetchInitialData();
+    }
+  }, [token, currentPage]);
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-      {movies.data &&
-        movies.data.map(({ id, title, posterUrl }) => {
-          return (
-            <Movie
-              key={id}
-              id={id}
-              token={token}
-              posterUrl={posterUrl}
-              title={title}
-            />
-          );
-        })}
-    </section>
+    <>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+        {movies.data &&
+          movies.data.map(({ id, title, posterUrl }) => {
+            return (
+              <Movie
+                key={id}
+                id={id}
+                token={token}
+                posterUrl={posterUrl}
+                title={title}
+              />
+            );
+          })}
+      </section>
+      <section>
+        <ResponsivePagination
+          current={currentPage}
+          total={movies.totalPages}
+          onPageChange={setCurrentPage}
+        />
+      </section>
+    </>
   );
 };
 
