cb3508bf71d0b7f3efb8adfee3d1cb8113cbe00c
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2024-05-02 21:36:37 -0400

    Refactor Details component

--- app/components/Movie/Details.tsx
+++ app/components/Movie/Details.tsx
@@ -8,32 +8,56 @@ const Details = ({ genres, rating, summary, title }: DetailsProps) => {
   return (
     <div className="bg-black flex flex-col gap-5 py-6 px-3 text-white h-[375px] overflow-y-auto rounded-2xl">
       <h2 className="text-left font-display text-xl font-bold">{title}</h2>
-      <p className="flex items-center justify-center w-fit min-w-24 h-6 border-white border-2 rounded-full font-display font-medium text-sm">
-        {rating}
-      </p>
-      <p className="text-left font-sans">{summary}</p>
-      {genres.length > 0 && (
-        <div className="flex flex-wrap items-center gap-x-2">
-          {genres.map(({ id, title }, index) => {
-            if (index === genres.length - 1) {
-              return (
-                <p key={id} className="font-display uppercase text-sm">
-                  {title}
-                </p>
-              );
-            }
-            return (
-              <>
-                <p key={id} className="font-display uppercase text-sm">
-                  {title}
-                </p>
-                <span>·</span>
-              </>
-            );
-          })}
-        </div>
-      )}
+      <Rating rating={rating} />
+      <Summary summary={summary} />
+      <Genres genres={genres} />
     </div>
   );
 };
 export default Details;
+
+const Rating = ({ rating }: { rating: string }) => {
+  if (!rating)
+    return <p className="text-left italic font-sans">Rating not found</p>;
+
+  return (
+    <p className="flex items-center justify-center w-fit min-w-24 h-6 border-white border-2 rounded-full font-display font-medium text-sm">
+      {rating}
+    </p>
+  );
+};
+
+const Summary = ({ summary }: { summary: string }) => {
+  if (!summary)
+    return (
+      <p className="text-left italic font-sans">Movie summary not found</p>
+    );
+
+  return <p className="text-left font-sans">{summary}</p>;
+};
+
+const Genres = ({ genres }: { genres: { id: string; title: string }[] }) => {
+  if (genres.length < 1) return null
+
+  return (
+    <div className="flex flex-wrap items-center gap-x-2">
+      {genres.map(({ id, title }, index) => {
+        if (index === genres.length - 1) {
+          return (
+            <p key={id} className="font-display uppercase text-sm">
+              {title}
+            </p>
+          );
+        }
+        return (
+          <>
+            <p key={id} className="font-display uppercase text-sm">
+              {title}
+            </p>
+            <span>·</span>
+          </>
+        );
+      })}
+    </div>
+  )
+}
